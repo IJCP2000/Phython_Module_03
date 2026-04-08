4da9eef23b2227f5c50e34e56184a667327c72b1
IJCP2000 <ijcp252000@gmail.com>
2026-04-08 16:00:42 +0100

    4

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -5,34 +5,34 @@ def get_coordinates(prompt) -> tuple:
     while True:
         user_input = input(f"{prompt}: ")
         try:
-            # Split by comma and strip whitespace
             parts = [p.strip() for p in user_input.split(',')]
+            # parts will contain the cord given in input
 
             if len(parts) != 3:
                 print("Invalid syntax")
                 continue
 
-            # Attempt to convert each part to float
-            # If one fails, we catch which one specifically for the error
-            coords = []
+            coordenates = []
+            # list to contain converted tuple
             for p in parts:
                 try:
-                    coords.append(float(p))
+                    coordenates.append(float(p))
+                    # try convert each part of the tuple to float
                 except ValueError as e:
                     print(f"Error on parameter '{p}': {e}")
-                    raise  # Break out of the inner loop to the outer except
+                    raise  # Break out for the other except
 
-            return tuple(coords)
+            return tuple(coordenates)
 
         except ValueError:
-            # This catches the re-raised error from the float conversion
+            # we need this a we use the while True to ask for cord inputs until
+            # a valid one is given
             continue
 
 
 def main():
     print("=== Game Coordinate System ===")
 
-    # First set of coordinates
     print("\nGet a first set of coordinates")
     c1 = get_coordinates("Enter new coordinates as floats in format 'x,y,z'")
 
@@ -43,12 +43,10 @@ def main():
     print(f"It includes: X={x1}, Y={y1}, Z={z1}")
     print(f"Distance to center: {dist_origin:.4f}")
 
-    # Second set of coordinates
     print("\nGet a second set of coordinates")
     c2 = get_coordinates("Enter new coordinates as floats in format 'x,y,z'")
 
     x2, y2, z2 = c2
-    # Distance formula: sqrt((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
     dist_between = math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
 
     print(f"Distance between the 2 sets of coordinates: {dist_between:.4f}")
